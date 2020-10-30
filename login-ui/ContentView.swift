36cc7ab7557274ecd554b628e70b315147d7ba14
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-10-31 00:08:08 +0100

    Initial Commit

---
+++ login-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login-ui
+//
+//  Created by Sergiu Waxmann on 31/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ login-ui/login_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_uiApp.swift
+//  login-ui
+//
+//  Created by Sergiu Waxmann on 31/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct login_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
