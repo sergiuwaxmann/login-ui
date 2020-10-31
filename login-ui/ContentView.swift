3445d91589df4747ddc7faec6e36a3d2f7d73ea9
Sergiu Waxmann <waxmann.sergiu@me.com>
2020-10-31 13:18:41 +0100

    Add Biometrics & Change Display Name

--- login-ui/ContentView.swift
+++ login-ui/ContentView.swift
@@ -4,14 +4,26 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    
+    @AppStorage("status") var logged = false
+    
     var body: some View {
         
         NavigationView {
-            Home()
-                .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
-                .navigationBarHidden(true)
+            
+            if logged {
+                Text("Hello World!")
+                    .navigationTitle("Home")
+                    .navigationBarHidden(false)
+                    .preferredColorScheme(.light)
+            } else {
+                Home()
+                    .preferredColorScheme(.dark)
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
@@ -27,6 +39,10 @@ struct Home : View {
     @State var username = ""
     @State var password = ""
     
+    // Store for future biometric login
+    @AppStorage("stored_User") var user = "waxmann.sergiu@me.com"
+    @AppStorage("status") var logged = false
+    
     var body: some View {
         VStack {
             
@@ -35,7 +51,7 @@ struct Home : View {
             Image("Logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 250)
+                .padding(.horizontal, 35)
                 .padding(.vertical)
             
             HStack {
@@ -54,6 +70,7 @@ struct Home : View {
                 Spacer(minLength: 0)
             }
             .padding()
+            .padding(.leading, 15)
             
             HStack {
                 
@@ -63,6 +80,7 @@ struct Home : View {
                     .frame(width: 35)
                 
                 TextField("EMAIL", text: $username)
+                    .autocapitalization(.none)
             }
             .padding()
             .background(Color.white.opacity(username == "" ? 0 : 0.12))
@@ -76,7 +94,8 @@ struct Home : View {
                     .foregroundColor(.white)
                     .frame(width: 35)
                 
-                TextField("PASSWORD", text: $password)
+                SecureField("PASSWORD", text: $password)
+                    .autocapitalization(.none)
             }
             .padding()
             .background(Color.white.opacity(password == "" ? 0 : 0.12))
@@ -84,18 +103,34 @@ struct Home : View {
             .padding(.horizontal)
             .padding(.top)
             
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Text("LOGIN")
-                    .fontWeight(.heavy)
-                    .foregroundColor(.black)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 150)
-                    .background(Color("GreenColor"))
-                    .clipShape(Capsule())
-            })
+            HStack(spacing: 15) {
+                
+                Button(action: {}, label: {
+                    Text("LOGIN")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.black)
+                        .padding(.vertical)
+                        .frame(width: UIScreen.main.bounds.width - 150)
+                        .background(Color("GreenColor"))
+                        .clipShape(Capsule())
+                })
+                .opacity(username != "" && password != "" ? 1 : 0.5)
+                .disabled(username != "" && password != "" ? false : true)
+                
+                if getBiometricsStatus() {
+                    Button(action: authenticateUser, label: {
+                        Image(systemName: LAContext().biometryType == .faceID ? "faceid" : "touchid")
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color("GreenColor"))
+                            .clipShape(Circle())
+                    })
+                }
+            }
             .padding(.top)
             
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Button(action: {}, label: {
                 Text("Forgot password?")
                     .foregroundColor(Color("GreenColor"))
             })
@@ -108,7 +143,7 @@ struct Home : View {
                 Text("Don't have an account?")
                     .foregroundColor(Color.white.opacity(0.6))
                 
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Button(action: {}, label: {
                     Text("Sign up")
                         .fontWeight(.heavy)
                         .foregroundColor(Color("GreenColor"))
@@ -117,5 +152,30 @@ struct Home : View {
             .padding(.vertical)
         }
         .background(Color("BackgroundColor").ignoresSafeArea(.all, edges: .all))
+        .animation(.easeOut)
     }
+    
+    func getBiometricsStatus()->Bool {
+        let scanner = LAContext()
+        if username == user && scanner.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: .none) {
+            return true
+        }
+        return false
+    }
+    
+    func authenticateUser() {
+        
+        let scanner = LAContext()
+        scanner.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "To Unlock \(username)") { (status, err) in
+            if err != nil {
+                print(err!.localizedDescription)
+                return
+            }
+            
+            withAnimation(.easeOut) {
+                logged = true
+            }
+        }
+    }
+    
 }
